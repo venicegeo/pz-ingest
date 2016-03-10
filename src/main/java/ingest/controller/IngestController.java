fe8164f9ff8e6d2bceb604e0bc65a7af88224478
go0ty <patrick.doody@rgi-corp.com>
2016-03-10 08:08:33 -0500

    Add admin/stats

---
+++ src/main/java/ingest/controller/IngestController.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright 2016, RadiantBlue Technologies, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+package ingest.controller;
+
+import ingest.messaging.IngestThreadManager;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * REST Controller for ingest. Ingest has no functional REST endpoints, as all
+ * communication is done through Kafka. However, this controller exposes useful
+ * debug/status endpoints which can be used administratively.
+ * 
+ * @author Patrick.Doody
+ * 
+ */
+@RestController
+public class IngestController {
+	@Autowired
+	private IngestThreadManager threadManager;
+
+	/**
+	 * Returns administrative statistics for this component.
+	 * 
+	 * @return Component information
+	 */
+	@RequestMapping(value = "/admin/stats", method = RequestMethod.GET)
+	public ResponseEntity<Map<String, Object>> getAdminStats() {
+		Map<String, Object> stats = new HashMap<String, Object>();
+		// Return information on the jobs currently being processed
+		stats.put("jobs", threadManager.getRunningJobIDs());
+		return new ResponseEntity<Map<String, Object>>(stats, HttpStatus.OK);
+	}
+}
