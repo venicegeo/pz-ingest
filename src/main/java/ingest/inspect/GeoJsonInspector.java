abd5e300a1312b5d945c83f48dbe7cb5364c1c19
go0ty <patrick.doody@rgi-corp.com>
2016-05-03 09:07:06 -0400

    Code cleanup+refactor, cleanup resources

--- src/main/java/ingest/inspect/GeoJsonInspector.java
+++ src/main/java/ingest/inspect/GeoJsonInspector.java
@@ -19,7 +19,6 @@ import ingest.utility.IngestUtilities;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -60,7 +59,6 @@ import org.opengis.feature.type.AttributeDescriptor;
 import org.opengis.feature.type.AttributeType;
 import org.opengis.feature.type.GeometryDescriptor;
 import org.opengis.feature.type.GeometryType;
-import org.opengis.filter.identity.FeatureId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -68,9 +66,12 @@ import org.springframework.stereotype.Component;
 import util.PiazzaLogger;
 
 /**
- * Inspects GeoJSON string and loads it into postGIS only if host is true.
+ * Inspects GeoJSON. Will parse the GeoJSON input to ensure validity, and parse
+ * information such as spatial bounding box.
  * 
- * @author Sonny.Saniev
+ * Vectors for GeoJSON will be stored in Piazza PostGIS table.
+ * 
+ * @author Sonny.Saniev, Patrick Doody
  * 
  */
 @Component
@@ -105,7 +106,8 @@ public class GeoJsonInspector implements InspectorType {
 		// Create local placeholder file for Shapefile contents
 		File localWriteDir = new File(String.format("%s%s", "tmp_output_", dataResource.getDataId()));
 		localWriteDir.mkdir();
-		File shapeFilePlaceHolder = new File(String.format("%s%s%s%s", localWriteDir.getAbsolutePath(), File.separator, dataResource.getDataId(), ".shp"));
+		File shapeFilePlaceHolder = new File(String.format("%s%s%s%s", localWriteDir.getAbsolutePath(), File.separator,
+				dataResource.getDataId(), ".shp"));
 		shapeFilePlaceHolder.createNewFile();
 
 		// Persist mapped Shapefile into the Piazza PostGIS Database.
@@ -115,7 +117,8 @@ public class GeoJsonInspector implements InspectorType {
 			convertGeoJsonToShapeFile(shapeFilePlaceHolder, dataResource);
 
 			// Persist to PostGIS
-			FeatureSource<SimpleFeatureType, SimpleFeature> shpFeatureSource = ingestUtilities.getShapefileDataStore(shapeFilePlaceHolder.getAbsolutePath());
+			FeatureSource<SimpleFeatureType, SimpleFeature> shpFeatureSource = ingestUtilities
+					.getShapefileDataStore(shapeFilePlaceHolder.getAbsolutePath());
 			ingestUtilities.persistShapeFile(shpFeatureSource, dataResource);
 
 			// Get the Bounding Box, set the Spatial Metadata
@@ -144,12 +147,17 @@ public class GeoJsonInspector implements InspectorType {
 
 		// Delete temporary Shapefile contents local temp folder
 		ingestUtilities.deleteDirectoryRecursive(shapeFilePlaceHolder.getParentFile());
+		// Delete the working directory.
+		localWriteDir.delete();
 
 		// Return DataResource
 		return dataResource;
 	}
 
 	/**
+	 * This method is required because GeoTools does not contain a GeoJSON Data
+	 * Store. Internally, we convert the GeoJSON to a Shapefile, which gets us a
+	 * Data Store that can be used to fully manipulate and parse the vectors.
 	 * 
 	 * @param shapefileOutput
 	 *            Shapefile file to map geojson into.
@@ -160,141 +168,135 @@ public class GeoJsonInspector implements InspectorType {
 	 * @throws Exception
 	 */
 	public File convertGeoJsonToShapeFile(File shapefileOutput, DataResource dataResource) throws Exception {
-
 		File geoJsonOriginalFile = getFile(dataResource);
 
-		// Mapping geojson to shapefile
+		// Mapping GeoJSON to Shapefile
 		ShapefileDataStoreFactory dataStoreFactory = new ShapefileDataStoreFactory();
 		Map<String, Serializable> params = new HashMap<String, Serializable>();
 		params.put("url", shapefileOutput.toURI().toURL());
 		params.put("create spatial index", Boolean.TRUE);
 		ShapefileDataStore shpDataStore = (ShapefileDataStore) dataStoreFactory.createNewDataStore(params);
-		
+
 		InputStream in = new FileInputStream(geoJsonOriginalFile);
 		int decimals = 15;
 		GeometryJSON gjson = new GeometryJSON(decimals);
 		FeatureJSON fjson = new FeatureJSON(gjson);
-		//FeatureCollection fc = fjson.readFeatureCollection(in);
-        FeatureCollection<SimpleFeatureType, SimpleFeature> fc = fjson.readFeatureCollection(in);
-        fc.getSchema();
-        
-        // write features to shape file
-        writeFeatures(fc, shpDataStore);
-        
+		FeatureCollection<SimpleFeatureType, SimpleFeature> fc = fjson.readFeatureCollection(in);
+		fc.getSchema();
+
+		// Write features to shape file
+		writeFeatures(fc, shpDataStore);
+
 		// clean up efforts
 		in.close();
 		ingestUtilities.deleteDirectoryRecursive(geoJsonOriginalFile.getParentFile());
 
 		return shapefileOutput;
 	}
-	
+
 	/**
-	 * Code snippet from https://gitlab.com/snippets/9275 to write features into
-	 * the shapefile
+	 * Writes features collected from GeoJSON into a Shapefile for internal
+	 * GeoTools Data Store parsing.
+	 * 
+	 * @see "https://gitlab.com/snippets/9275"
 	 * 
 	 * @param features
 	 *            FeatureCollection parameter
+	 * @param shpDataStore
+	 *            The shapefile data store to pack the GeoJSON features into.
 	 * @return
 	 * @throws Exception
 	 */
-	@SuppressWarnings("resource")
-	private boolean writeFeatures(FeatureCollection<SimpleFeatureType, SimpleFeature> features, ShapefileDataStore shpDataStore) throws Exception {
-
+	private void writeFeatures(FeatureCollection<SimpleFeatureType, SimpleFeature> features,
+			ShapefileDataStore shpDataStore) throws Exception {
+		// Ensure the Data Store is valid before proceeding.
 		if (shpDataStore == null) {
-			throw new IllegalStateException("Datastore can not be null when writing");
+			throw new IllegalStateException("GeoJson inspection error: Temporary Shapefile Store is not accessible.");
 		}
+
 		SimpleFeatureType schema = features.getSchema();
 		GeometryDescriptor geom = schema.getGeometryDescriptor();
 		String oldGeomAttrib = "";
-		try {
 
-			// Write the features to the shapefile
-			Transaction transaction = new DefaultTransaction("create");
+		// Write the features to the shapefile
+		Transaction transaction = new DefaultTransaction("create");
 
-			/*
-			 * The Shapefile format has a couple limitations: - "the_geom" is
-			 * always first, and used for the geometry attribute name -
-			 * "the_geom" must be of type Point, MultiPoint, MuiltiLineString,
-			 * MultiPolygon - Attribute names are limited in length - Not all
-			 * data types are supported (example Timestamp represented as Date)
-			 *
-			 * Because of this we have to rename the geometry element and then
-			 * rebuild the features to make sure that it is the first attribute.
-			 */
-			List<AttributeDescriptor> attributes = schema.getAttributeDescriptors();
-			GeometryType geomType = null;
-			List<AttributeDescriptor> attribs = new ArrayList<AttributeDescriptor>();
-			for (AttributeDescriptor attrib : attributes) {
-				AttributeType type = attrib.getType();
-				if (type instanceof GeometryType) {
-					geomType = (GeometryType) type;
-					oldGeomAttrib = attrib.getLocalName();
-				} else {
-					attribs.add(attrib);
-				}
-			}
-
-			GeometryTypeImpl gt = new GeometryTypeImpl(new NameImpl("the_geom"), geomType.getBinding(),
-					geomType.getCoordinateReferenceSystem(), geomType.isIdentified(), geomType.isAbstract(), geomType.getRestrictions(),
-					geomType.getSuper(), geomType.getDescription());
-
-			GeometryDescriptor geomDesc = new GeometryDescriptorImpl(gt, new NameImpl("the_geom"), geom.getMinOccurs(), geom.getMaxOccurs(),
-					geom.isNillable(), geom.getDefaultValue());
-
-			attribs.add(0, geomDesc);
-
-			SimpleFeatureType shpType = new SimpleFeatureTypeImpl(schema.getName(), attribs, geomDesc, schema.isAbstract(),
-					schema.getRestrictions(), schema.getSuper(), schema.getDescription());
-
-			shpDataStore.createSchema(shpType);
-
-			String typeName = shpDataStore.getTypeNames()[0];
-			SimpleFeatureSource featureSource = shpDataStore.getFeatureSource(typeName);
-
-			// if (featureSource instanceof SimpleFeatureStore)
-			if (featureSource instanceof FeatureStore) {
-
-				SimpleFeatureStore featureStore = (SimpleFeatureStore) featureSource;
-				List<SimpleFeature> feats = new ArrayList<SimpleFeature>();
-
-				FeatureIterator<SimpleFeature> features2 = features.features();
-				while (features2.hasNext()) {
-					SimpleFeature f = features2.next();
-					SimpleFeature reType = DataUtilities.reType(shpType, f, true);
-					// set the default Geom (the_geom) from the original Geom
-					reType.setAttribute("the_geom", f.getAttribute(oldGeomAttrib));
-
-					feats.add(reType);
-				}
-				features2.close();
-				SimpleFeatureCollection collection = new ListFeatureCollection(shpType, feats);
-
-				featureStore.setTransaction(transaction);
-				try {
-					List<FeatureId> ids = featureStore.addFeatures(collection);
-					transaction.commit();
-				} catch (Exception problem) {
-					problem.printStackTrace();
-					transaction.rollback();
-				} finally {
-					transaction.close();
-				}
-				shpDataStore.dispose();
-				return true;
+		/*
+		 * The Shapefile format has a couple limitations: - "the_geom" is always
+		 * first, and used for the geometry attribute name - "the_geom" must be
+		 * of type Point, MultiPoint, MuiltiLineString, MultiPolygon - Attribute
+		 * names are limited in length - Not all data types are supported
+		 * (example Timestamp represented as Date)
+		 * 
+		 * Because of this we have to rename the geometry element and then
+		 * rebuild the features to make sure that it is the first attribute.
+		 */
+		List<AttributeDescriptor> attributes = schema.getAttributeDescriptors();
+		GeometryType geomType = null;
+		List<AttributeDescriptor> attribs = new ArrayList<AttributeDescriptor>();
+		for (AttributeDescriptor attrib : attributes) {
+			AttributeType type = attrib.getType();
+			if (type instanceof GeometryType) {
+				geomType = (GeometryType) type;
+				oldGeomAttrib = attrib.getLocalName();
 			} else {
-				shpDataStore.dispose();
-				System.err.println("ShapefileStore not writable");
-				logger.log("GeoJson to mapping exception occurred, temporary ShapefileStore not writable", PiazzaLogger.ERROR);
-
-				throw new Exception("GeoJson to mapping exception occurred, temporary ShapefileStore not writable");
+				attribs.add(attrib);
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
-		return false;
+
+		GeometryTypeImpl gt = new GeometryTypeImpl(new NameImpl("the_geom"), geomType.getBinding(),
+				geomType.getCoordinateReferenceSystem(), geomType.isIdentified(), geomType.isAbstract(),
+				geomType.getRestrictions(), geomType.getSuper(), geomType.getDescription());
+		GeometryDescriptor geomDesc = new GeometryDescriptorImpl(gt, new NameImpl("the_geom"), geom.getMinOccurs(),
+				geom.getMaxOccurs(), geom.isNillable(), geom.getDefaultValue());
+		attribs.add(0, geomDesc);
+
+		SimpleFeatureType shpType = new SimpleFeatureTypeImpl(schema.getName(), attribs, geomDesc, schema.isAbstract(),
+				schema.getRestrictions(), schema.getSuper(), schema.getDescription());
+		shpDataStore.createSchema(shpType);
+
+		String typeName = shpDataStore.getTypeNames()[0];
+		SimpleFeatureSource featureSource = shpDataStore.getFeatureSource(typeName);
+
+		if (featureSource instanceof FeatureStore) {
+			SimpleFeatureStore featureStore = (SimpleFeatureStore) featureSource;
+			List<SimpleFeature> feats = new ArrayList<SimpleFeature>();
+
+			FeatureIterator<SimpleFeature> features2 = features.features();
+			while (features2.hasNext()) {
+				SimpleFeature f = features2.next();
+				SimpleFeature reType = DataUtilities.reType(shpType, f, true);
+				// set the default Geom (the_geom) from the original Geom
+				reType.setAttribute("the_geom", f.getAttribute(oldGeomAttrib));
+				feats.add(reType);
+			}
+			features2.close();
+			SimpleFeatureCollection collection = new ListFeatureCollection(shpType, feats);
+
+			featureStore.setTransaction(transaction);
+			try {
+				featureStore.addFeatures(collection);
+				transaction.commit();
+			} catch (Exception problem) {
+				problem.printStackTrace();
+				transaction.rollback();
+			} finally {
+				transaction.close();
+			}
+			shpDataStore.dispose();
+		} else {
+			// Clean up
+			shpDataStore.dispose();
+			transaction.close();
+			// Log the error
+			String error = "GeoJson inspection error: Temporary Shapefile Store is not writable.";
+			logger.log(error, PiazzaLogger.ERROR);
+			throw new Exception(error);
+		}
 	}
 
 	/**
+	 * Gets the File for a Data Resource.
 	 * 
 	 * @param dataResource
 	 *            data resource to pull file from
