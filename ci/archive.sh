beea411920ec2f46162a7c0374d87ea0472cc6a0
sbaxter <seth@neuralux.com>
2016-12-06 09:46:24 -0500

    Debug

--- ci/archive.sh
+++ ci/archive.sh
@@ -10,6 +10,13 @@ source $root/ci/vars.sh
 # Path to output JAR
 src=$root/target/piazza-ingest*.jar
 
+umask
+umask 0
+umask
+
+chmod 755 $root
+chmod 644 $root/src/test/resources/*
+
 # Build Spring-boot JAR
 [ -f $src ] || mvn clean package -U
 
