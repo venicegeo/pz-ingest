cf7bda7b8df9d72914994850795d64adf5d38e21
Patrick D <go0ty@users.noreply.github.com>
2017-11-08 11:26:32 -0500

    Configurable KMS (#39)
    * Configurable KMS
    
    * Use Boolean Value Check

--- src/main/java/ingest/inspect/ShapefileInspector.java
+++ src/main/java/ingest/inspect/ShapefileInspector.java
@@ -72,8 +72,6 @@ public class ShapefileInspector implements InspectorType {
 	private String AMAZONS3_ACCESS_KEY;
 	@Value("${vcap.services.pz-blobstore.credentials.secret_access_key}")
 	private String AMAZONS3_PRIVATE_KEY;
-	@Value("${vcap.services.pz-blobstore.credentials.encryption_key}")
-	private String KMS_CMK_ID;
 
 	@Autowired
 	private PiazzaLogger logger;
